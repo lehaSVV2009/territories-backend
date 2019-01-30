98c53efa8ebc36b65927152909a72af81dcc0a65
Alex Soroka <lehaSVV2009@gmail.com>
2019-01-30 22:41:24 +0530

    Use heroku api key variable to deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Server } from "./libs/boardgame.io/server";
 import { Game } from "territories-core";
 
 const port = process.env.PORT || 8000;
-const server = Server({ games: [Game({ board: null, dices: null })] });
+const server = Server({ games: [Game({})] });
 
 server.run(port, () => {
   console.log("App is running on port " + port);
