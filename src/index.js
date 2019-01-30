2507f742a842a837f100263ca5ab29e019736c10
Alex Soroka <lehaSVV2009@gmail.com>
2019-01-30 22:24:36 +0530

    Simplify game usage

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import { Server } from "./libs/boardgame.io/server";
 import { Game } from "territories-core";
 
 const port = process.env.PORT || 8000;
-const game = Game({ board: null, dices: null });
-const server = Server({ games: [game] });
+const server = Server({ games: [Game({ board: null, dices: null })] });
 
 server.run(port, () => {
   console.log("App is running on port " + port);
